9f50582152eccbeca4daa3799c5e3dfb280950fe
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-27 22:09:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,12 @@
 class Book
   @@all = []
     attr_accessor :author, :page_count, :genre
+    attr_reader :title
     
   def initialize(title)
     @title = title
     @@all << @title 
   end 
-  
-  def title
-   @title
-  end
-
  
  def turn_page
    puts "Flipping the page...wow, you read fast!"
