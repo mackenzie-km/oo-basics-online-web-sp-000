3edb31f75bf5b22259628d5463546a59202223eb
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-27 21:50:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  @@all = []
+    attr_accessor :author, :page_count, :genre
+  
+  def title
+    #pick up here!
+  end
+  
+  def initialize(title)
+    @title = title
+    @@all << @title 
+  end 
 
+ 
+ def turn_page
+   puts "Flipping the page...wow, you read fast!"
+end
+  
+end
 
